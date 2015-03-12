3d2343bd2d5bebdfe9965f7d69843a46700484be
houhai.duan <houhai.duan@happyelements.com>
2015-03-12 18:24:18 +0800

    t

--- convert/test.js
+++ convert/test.js
@@ -1,17 +1,17 @@
 var fs=require("fs");
 var ConvertFca=require("./ConvertFca");
 
-var content=fs.readFileSync("temp/DR.json");
+var content=fs.readFileSync("temp/cha/Phoenix/cha.json");
 var data=JSON.parse(content);
 
 var convert=new ConvertFca(data,true);
 
-//var baseLayers=convert.makeBaseLayers(data.actions[7]);
+var baseLayers=convert.makeBaseLayers(data.actions[8]);
 //console.log(baseLayers);
 //fs.writeFileSync("temp/b.js","var b="+JSON.stringify(baseLayers,null,4)+";");
 
-//var action=convert.convertAction(data.actions[3]);
+//var action=convert.convertAction(data.actions[8]);
 
-var actions=convert.convertActions();
-fs.writeFileSync("temp/t.js","var a="+JSON.stringify(actions,null,4)+";");
+//var actions=convert.convertActions();
+//fs.writeFileSync("temp/t.js","var a="+JSON.stringify(actions,null,4)+";");
 
