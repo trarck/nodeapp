251a2923c4db0d36e3c74a833747ac697e9b6600
duanhouhai <houhai.duan@mihoyo.com>
2021-01-08 20:02:26 +0800

    封装成函数。

--- casc/version.js
+++ casc/version.js
@@ -21,6 +21,16 @@ var opts= [
 	   full: 'out', 
 	   abbr: 'o',
 	   defaultValue:""
+   }, 
+   { 
+	   full: 'startVersion', 
+	   abbr: 'sv',
+	   defaultValue:1
+   }, 
+   { 
+	   full: 'endVersion', 
+	   abbr: 'ev',
+	   defaultValue:null
    }
 ];
 
@@ -28,40 +38,58 @@ var result=ArgParser.parse(opts);
 
 var srcPath=result.options.src || "data";
 var outPath=result.options.out || "out";
-var action = result.cmds[0];
+var startVersion=result.options.startVersion || 1;
+
 mkdirs(outPath);
 
 var idxGroup = IndexFile.getIdxFiles(srcPath);
 for(var i in idxGroup){
+    idxGroup[i].sort(IndexFile.sortIdxByVersionAsc);
     IndexFile.loadAllIdxVersionEntries(idxGroup[i])
 }
 
-var sameVersionIdxs=[];
-var version=1;
+var maxVersion=0;
+
 for(var i in idxGroup){
     var idxs=idxGroup[i];
-    sameVersionIdxs.push(idxs[version-1]);
-}
-console.log(sameVersionIdxs.length);
-
-var sameVersionEntries=[];
-for(var i in sameVersionIdxs){
-    sameVersionEntries=sameVersionEntries.concat(sameVersionIdxs[i].entries);
-    console.log(sameVersionEntries.length,sameVersionIdxs[i].entries.length);
-}
-
-sameVersionEntries.sort(IndexFile.sortIdxEntries);
-var versionPath=path.join(outPath,version+".csv");
-Csv.writeAll(versionPath,sameVersionEntries);
-
-function outCsvData(filePath,arr){
-    console.log("export",filePath);
-    var cnt="";
-    for(var i in arr){
-        for(var k in arr[i]){
-            cnt+=arr[i][k]+","
-        }
-        cnt+="\n";
+    if(idxs.length>maxVersion){
+        maxVersion=idxs.length;
     }
-    fs.writeFileSync(filePath,cnt);
+}
+console.log("maxVersion=",maxVersion);
+
+var endVersion=result.options.endVersion || maxVersion;
+exportVersionData(startVersion,endVersion);
+
+function exportVersionData(startVersion,endVersion){
+    for(var i=startVersion;i<endVersion;++i){
+        var sameVersionIdxs=getVersionIdxs(i,idxGroup);
+        exportVersionEntries(i,sameVersionIdxs);
+    }
+}
+
+
+function exportVersionEntries(version,idxs){
+    console.log("export version "+version+" entries");
+    var sameVersionEntries=[];
+    for(var i in idxs){
+        sameVersionEntries=sameVersionEntries.concat(idxs[i].entries);
+    } 
+    
+    sameVersionEntries.sort(IndexFile.sortIdxEntries);
+    var versionPath=path.join(outPath,version+".csv");
+    Csv.writeAll(versionPath,sameVersionEntries);
+}
+
+function getVersionIdxs(version,idxGroup){
+    console.log("collect version "+version+" idx");
+    var sameVersionIdxs=[];
+    for(var i in idxGroup){
+        var idxs=idxGroup[i];
+        if(version>idxs.length){
+            version = idxs.length;
+        }
+        sameVersionIdxs.push(idxs[version-1]);
+    }
+    return sameVersionIdxs;
 }
