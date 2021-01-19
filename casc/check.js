896bdb299f473de96dfcd26f0f54fd0a7805d221
trarck <trarck@gmail.com>
2021-01-19 10:27:44 +0800

    temp

--- casc/check.js
+++ casc/check.js
@@ -8,6 +8,7 @@ var Task=yhnode.async.Task;
 var WorkPool=yhnode.async.WorkPool;
 
 var IndexFile=require("./IndexFile.js").IndexFile;
+var Csv=require("./Csv.js");
 
 
 var opts= [
@@ -20,11 +21,6 @@ var opts= [
 	   full: 'out', 
 	   abbr: 'o',
 	   defaultValue:""
-   }, 
-   { 
-	   full: 'model', 
-	   abbr: 'm',
-	   defaultValue:"check"
    }
 ];
 
@@ -42,21 +38,36 @@ for(var i in idxGroup){
 switch(action){
     case "check":
         checkIdxVersion(idxGroup);
+        checkDuplicate(idxGroup);
         break;
 }
 
+
+function checkDuplicate(idxGroup){
+    for(var i in idxGroup){
+        for(var j in idxGroup[i]){
+            var idxInfo=idxGroup[i][j];
+            var dups = getVersionDuplicate(idxInfo);
+            if(dups && dups.length>0){
+                var dupFileName=IndexFile.getFileName(idxInfo)+".csv";
+                Csv.writeAll(path.join(outPath,dupFileName),dups);
+            }
+        }
+    }
+}
 
 function checkIdxVersion(idxGroup){
     for(var i in idxGroup){
         checkSameIdxVersion(idxGroup[i])
     }
 }
+
 function checkSameIdxVersion(idxs){
     for(var i=0;i<idxs.length-1;++i){
-        console.log("check",idxs[i].type,idxs[i].version,idxs[i+1].version);
+        //console.log("check",idxs[i].type,idxs[i].version,idxs[i+1].version);
         var diff=diffIdxVersion(idxs[i],idxs[i+1]);
         if(diff.updates.length>0){
-            console.log("updates ",diff.updates.length,idxs[i].type,idxs[i].version,idxs[i+1].version);
+            //console.log("updates ",diff.updates.length,idxs[i].type,idxs[i].version,idxs[i+1].version);
             var updates=[]
             for(var k in diff.updates){
                 var u={
@@ -76,14 +87,14 @@ function checkSameIdxVersion(idxs){
             var fromIdxName=IndexFile.getFileName(idxs[i]);
             var toIdxName=IndexFile.getFileName(idxs[i+1]);
             var removeFileName=fromIdxName+"--"+toIdxName+".updates.csv";
-            outCsvData(path.join(outPath,removeFileName),updates);
+            Csv.writeAll(path.join(outPath,removeFileName),updates);
         }
         if(diff.removes.length>0){
-            console.log("remove ",diff.removes.length,idxs[i].type,idxs[i].version,idxs[i+1].version);
+            //console.log("remove ",diff.removes.length,idxs[i].type,idxs[i].version,idxs[i+1].version);
             var fromIdxName=IndexFile.getFileName(idxs[i]);
             var toIdxName=IndexFile.getFileName(idxs[i+1]);
             var removeFileName=fromIdxName+"--"+toIdxName+".removes.csv";
-            outCsvData(path.join(outPath,removeFileName),diff.removes);
+            Csv.writeAll(path.join(outPath,removeFileName),diff.removes);
         }
     }
 }
@@ -99,7 +110,9 @@ function diffIdxVersion(verA,verB){
         var entry=verA.entries[i];
         
         keys[entry.key] = true;
-        aEntries[entry.key]=entry;
+        if(!aEntries[entry.key]){
+            aEntries[entry.key]=entry;
+        }
     }
        
     for(var i in verB.entries){
@@ -127,14 +140,22 @@ function diffIdxVersion(verA,verB){
     return {adds:adds,updates:updates,removes:removes};
 }
 
-function outCsvData(filePath,arr){
-    console.log("export",filePath);
-    var cnt="";
-    for(var i in arr){
-        for(var k in arr[i]){
-            cnt+=arr[i][k]+","
+
+function getVersionDuplicate(idxInfo){
+    var keys={};
+    for(var i in idxInfo.entries){
+        var entry=idxInfo.entries[i];
+        if(!keys[entry.key]){
+            keys[entry.key]=[];
         }
-        cnt+="\n";
+        keys[entry.key].push(entry);
     }
-    fs.writeFileSync(filePath,cnt);
+    
+    var dups=[];
+    for(var k in keys){
+        if(keys[k].length>1){
+            dups=dups.concat(keys[k]);
+        }
+    }
+    return dups;
 }
